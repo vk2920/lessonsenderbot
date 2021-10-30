6ac2b6c3f2778e5e31e6f4ff9c8613b1b5428647
vk2920 <vk2920@yandex.ru>
2021-10-30 19:06:59 +0300

    Исправления в логировании

--- debugs.py
+++ debugs.py
@@ -1,9 +1,12 @@
+import logging
+
 from aiogram.types import Message
+
 
 def debug_log(message: Message, debug_message: str = "Дополнительное сообщение не указано"):
     user_id = message.from_user.id
     chat_id = message.chat.id
     text = message.text
     username = message.from_user.first_name
-    print(f"{username} ({user_id}) написал: {text} в чат {chat_id}")
-    print(debug_message)
+    logging.info(f"{username} ({user_id}) написал: {text} в чат {chat_id}")
+    logging.info(debug_message)
