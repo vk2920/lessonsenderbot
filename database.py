05a3b6b18e87d88f8250415eace3a01aaaf26e98
vk2920 <vk2920@yandex.ru>
2021-10-31 12:45:07 +0300

    Обработка ошибки связи с БД

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
+import logging
 import os
 import sys
+from goto import with_goto
 
 import psycopg
 
@@ -9,9 +11,12 @@ class DataBase:
         try:
             self._connection = psycopg.connect(conninfo=os.environ['DATABASE_URL'])
         except Exception as _ex:
-            print("ERROR: ", _ex)
+            logging.error("Ошибка подключения к БД, возможные причины чуть ниже")
+            logging.error("    1. Траблы с подключением")
+            logging.error("    2. Траблы с данными в переменных окружения (или их нет)")
             sys.exit()
 
+    @with_goto
     def r_get_pairs_by_group(self, day_of_week: int, even_week: bool, group: str):
         """
         :param day_of_week: порядковый номер дня недели (int, 1~6)
@@ -24,31 +29,52 @@ class DataBase:
              -- о — очная форма обучения (з — заочная)
         :return: список пар на запрошенный день
         """
-        pairs_list = []
-        with self._connection.cursor() as cur:
-            sql = f"SELECT * FROM public.pairs WHERE " + \
-                  f"group_name = '{group}' AND even_week = {even_week} AND " + \
-                  f"day_of_week = {day_of_week} ORDER BY ordinal"
-            cur.execute(sql)
-            for row in cur.fetchall():
-                pairs_list.append(row)
-        return pairs_list
+        label .r_gpbg_begin
+        try:
+            pairs_list = []
+            with self._connection.cursor() as cur:
+                sql = f"SELECT * FROM public.pairs WHERE " + \
+                      f"group_name = '{group}' AND even_week = {even_week} AND " + \
+                      f"day_of_week = {day_of_week} ORDER BY ordinal"
+                cur.execute(sql)
+                for row in cur.fetchall():
+                    pairs_list.append(row)
+            return pairs_list
+        except psycopg.OperationalError as _ex:
+            logging.error("Ошибка подключения, переподключение...")
+            # Реинициализация объекта для переподключения к БД
+            self.__init__()
+            goto .r_gpbg_begin
 
     def r_get_user_group(self, tg_id: int):
-        with self._connection.cursor() as cur:
-            cur.execute(f"""SELECT group_name FROM public.users WHERE tg_id = {tg_id} LIMIT 1""")
-            group = list(cur.fetchone())[0]
-            return group
+        label .r_gug_begin
+        try:
+            with self._connection.cursor() as cur:
+                cur.execute(f"""SELECT group_name FROM public.users WHERE tg_id = {tg_id} LIMIT 1""")
+                group = list(cur.fetchone())[0]
+                return group
+        except psycopg.OperationalError as _ex:
+            logging.error("Ошибка подключения, переподключение...")
+            # Реинициализация объекта для переподключения к БД
+            self.__init__()
+            goto .r_gug_begin
 
     def r_get_random_chat_id(self):
-        with self._connection.cursor() as cur:
-            sql = "SELECT chat_id FROM public.users ORDER BY RANDOM() LIMIT 1"
-            cur.execute(sql)
-            chat_id = list(cur.fetchone())[0]
-            if chat_id is not None and chat_id != "" and chat_id != 0:
-                return int(chat_id)
-            else:
-                return 0
+        label .r_grci_begin
+        try:
+            with self._connection.cursor() as cur:
+                sql = "SELECT chat_id FROM public.users ORDER BY RANDOM() LIMIT 1"
+                cur.execute(sql)
+                chat_id = list(cur.fetchone())[0]
+                if chat_id is not None and chat_id != "" and chat_id != 0:
+                    return int(chat_id)
+                else:
+                    return 0
+        except psycopg.OperationalError as _ex:
+            logging.error("Ошибка подключения, переподключение...")
+            # Реинициализация объекта для переподключения к БД
+            self.__init__()
+            goto .r_grci_begin
 
     def w_set_chat_id(self, user_id: int, chat_id: int):
         """
@@ -56,11 +82,18 @@ class DataBase:
         :param chat_id: ID чата с данным пользователем
         :return: True, если запись произведена успешно, иначе False
         """
-        with self._connection.cursor() as cur:
-            sql = f"UPDATE public.users SET chat_id = {chat_id} WHERE tg_id = {user_id}"
-            cur.execute(sql)
-        self._connection.commit()
-        return True
+        label .w_sci_begin
+        try:
+            with self._connection.cursor() as cur:
+                sql = f"UPDATE public.users SET chat_id = {chat_id} WHERE tg_id = {user_id}"
+                cur.execute(sql)
+            self._connection.commit()
+            return True
+        except psycopg.OperationalError as _ex:
+            logging.error("Ошибка подключения, переподключение...")
+            # Реинициализация объекта для переподключения к БД
+            self.__init__()
+            goto .w_sci_begin
 
     def w_register_user_by_id(self, tg_id: int, name: str, group: str):
         """
@@ -69,21 +102,35 @@ class DataBase:
         :param group: группа пользователя в формате
         :return: True, если запись произведена успешно, иначе False
         """
-        with self._connection.cursor() as cur:
-            sql = f"SELECT tg_id FROM public.users WHERE tg_id = {tg_id}"
-            cur.execute(sql)
-            if len(list(cur.fetchall())) != 0:
-                sql = f"UPDATE public.users SET group_name = '{group}' WHERE tg_id = {tg_id}"
-            else:
-                sql = f"INSERT INTO public.users (tg_id, name, group_name) VALUES ({tg_id}, '{name}', '{group}');"
-            cur.execute(sql)
-            self._connection.commit()
-        return True
+        label .w_rubi_begin
+        try:
+            with self._connection.cursor() as cur:
+                sql = f"SELECT tg_id FROM public.users WHERE tg_id = {tg_id}"
+                cur.execute(sql)
+                if len(list(cur.fetchall())) != 0:
+                    sql = f"UPDATE public.users SET group_name = '{group}' WHERE tg_id = {tg_id}"
+                else:
+                    sql = f"INSERT INTO public.users (tg_id, name, group_name) VALUES ({tg_id}, '{name}', '{group}');"
+                cur.execute(sql)
+                self._connection.commit()
+            return True
+        except psycopg.OperationalError as _ex:
+            logging.error("Ошибка подключения, переподключение...")
+            # Реинициализация объекта для переподключения к БД
+            self.__init__()
+            goto .w_rubi_begin
 
     # Административные метода класса DataBase
     def w_remove_pair_by_pair_id(self, pair_id: int):
-        with self._connection.cursor() as cur:
-            sql = f"DELETE FROM public.pairs WHERE id = {pair_id}"
-            cur.execute(sql)
-            self._connection.commit()
-        return True
+        label .w_rpbpi_begin
+        try:
+            with self._connection.cursor() as cur:
+                sql = f"DELETE FROM public.pairs WHERE id = {pair_id}"
+                cur.execute(sql)
+                self._connection.commit()
+            return True
+        except psycopg.OperationalError as _ex:
+            logging.error("Ошибка подключения, переподключение...")
+            # Реинициализация объекта для переподключения к БД
+            self.__init__()
+            goto .w_rpbpi_begin
