8b253bef490c71778d99cc86ccd2c497fa46523c
vk2920 <vk2920@yandex.ru>
2021-12-24 20:30:06 +0300

    Некоторые обновления * Добавлены группы ГПИ * Добавлен параметр для отключения авторассылки цитат
    Это последнее обновление за 2021 год
    С наступающим)

--- main.py
+++ main.py
@@ -557,7 +557,7 @@ async def command_execute(message: types.Message):
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(auto_pairs_sender())
-    # loop.create_task(auto_phrase_sender())
+    # loop.create_task(auto_pairs_sender())
+    loop.create_task(auto_phrase_sender())
 
     executor.start_polling(dp, skip_updates=True)
